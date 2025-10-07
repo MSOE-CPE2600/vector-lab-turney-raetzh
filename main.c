ceece7f92880c86b3223cfeb173804e499e57971
raetzh <raetzh@msoe.edu>
2025-10-07 10:36:25 -0500

    Initial Version

---
+++ main.c
@@ -0,0 +1,24 @@
+/**********************
+* Name: main.c
+* Desc: Main i/o, passes things to and calls on arraymgr
+* Auth: Holly Raetz
+* Date: Sept 30, 2025
+*
+* Comp: make clean
+**********************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char in[30] = "";
+    while(strcmp(in,"quit\n"))
+    {
+        printf("vectcalc> ");
+        fgets(in, 30, stdin);
+        printf("%s", in);
+    }
+
+    return 0;
+}
